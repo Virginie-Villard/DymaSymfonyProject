3ae810d27ca6e3d3da4f8a95a31949b3acb53bd1
virginie <vvillard@antidots-group.com>
2023-09-18 13:18:05 +0200

    Add function getSong in SongController.php

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -2,7 +2,15 @@
 
 namespace App\Controller;
 
-class SongController
-{
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+class SongController extends AbstractController
+{
+    #[Route('/api/songs/{id}')]
+    public function getSong($id) : Response
+    {
+        dd($id);
+    }
 }
